3a29754097043ca36d1152de377e23e18119b175
Marc d'Entremont <cramhead@gmail.com>
2025-01-19 13:45:18 -0800

    Augmented e2e test

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -6,4 +6,10 @@ test("homepage has title and links to intro page", async ({ page }) => {
   await expect(page).toHaveTitle("Welcome to Leptos");
 
   await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
+
+  await expect(page.getByRole('button')).toHaveText("Click Me: 0");
+
+  await page.click('button');
+
+  await expect(page.getByRole('button')).toHaveText("Click Me: 1");
 });
